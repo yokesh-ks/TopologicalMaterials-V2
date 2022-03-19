8ad230ba23a2824d7d3d57bf3a60d2b1d21e715b
yokesh-ks <ksyokesh98@gmail.com>
2022-03-19 23:15:07 +0530

    feat(periodic table): periodic Table data is displayed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
+import React, { useEffect } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Header from "../src/components/Header";
+import { periodicData } from "../utils/api";
+import PeriodicTable from "../src/components/PeriodicTable";
 
 export default function Home() {
+  const [allPeriodic, setAllPeriodic] = React.useState([]);
+  const [selectedElement, setSelectedElement] = React.useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await periodicData();
+      setAllPeriodic(data);
+    })();
+  }, []);
+
+  const handleElement = (e) => {
+    const element = e.target.innerText;
+    let filterData = [...selectedElement];
+    if (!selectedElement.includes(element)) {
+      filterData.push(element);
+    } else {
+      filterData = filterData.filter((item) => item !== element);
+    }
+    setSelectedElement(filterData);
+  };
+
   return (
-    <div className={styles.container}>
-      <p>Topological Materials</p>
-    </div>
+    <>
+      <Head>
+        <title>Yokesh Tools</title>
+        <meta name="description" content="Tools" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <div style={{ marginTop: 100 }}></div>
+      <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
+        <PeriodicTable
+          data={allPeriodic}
+          handleElement={handleElement}
+          selectedElement={selectedElement}
+        />
+      </div>
+    </>
   );
 }
