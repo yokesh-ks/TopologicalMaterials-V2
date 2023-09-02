692a2de2d6e21d2caed9d34891230618a8dcaa45
yokesh_ks <yokesh.ks@increscotech.com>
2023-09-02 21:21:56 +0530

    feat(theme switch): theme switch and components ui added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { ModeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
     <header className="fixed top-0 right-0 left-0 w-full h-[70px] z-10">
-      <nav className="flex items-center h-full">
+      <nav className="flex items-center h-full justify-between">
         <h2 className="font-bold text-2xl">Topological Materials</h2>
+
+        <div>
+          <ModeToggle />
+        </div>
       </nav>
     </header>
   );
